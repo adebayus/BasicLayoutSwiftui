9f09903e068ec80afd236cc4b1364357fcd32e4f
adhityabagasmiwa98 <adhitya@refactory.id>
2023-09-02 10:20:43 +0700

    Initial Commit

---
+++ BasicLayoutRfc/BasicLayoutRfcApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicLayoutRfcApp.swift
+//  BasicLayoutRfc
+//
+//  Created by refactory-mac on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BasicLayoutRfcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
